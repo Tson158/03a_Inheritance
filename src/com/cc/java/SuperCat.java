0ec5d36d935a46ff197aa0f70871c67c641177fb
Kakarott <Sonntag_thomas_86@web.de>
2021-12-06 10:22:57 +0100

    SuperCat ++

---
+++ src/com/cc/java/SuperCat.java
@@ -0,0 +1,49 @@
+package com.cc.java;
+
+public class SuperCat {
+    
+    private String name;
+    private String furColor;
+    private int age;
+
+    private int counter;
+
+    public SuperCat(String name, String furColor, int age) {
+        this.name = name;
+        this.furColor = furColor;
+        this.age = age;
+    }
+
+    public String getStringAttributes(String flag){
+        switch (flag) {
+            case "#name":
+                return name;
+            case "#color":
+                return furColor;
+            default:
+                return "#!?";
+        }  
+    }
+
+    public String getAge(){
+        return checkEscalationLevel();
+    }
+
+
+    private String checkEscalationLevel(){
+
+        counter++;
+
+        switch (counter) {
+            case 1:
+                return "This is an inappropriate question!"; 
+            case 2:
+                return "I've told you once!"; 
+            case 3:
+                return "Talk to the hand!"; 
+            default:
+                return "1#!?&&%"; 
+        } 
+    }
+
+}
